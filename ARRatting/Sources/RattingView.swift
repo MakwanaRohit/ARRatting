d5c0f4075d974e9ef43431979efd9021933a1f0c
Rohit Makwana <>
2020-04-21 16:09:29 +0530

    Added Ratting View

---
+++ ARRatting/Sources/RattingView.swift
@@ -0,0 +1,21 @@
+//
+//  RattingView.swift
+//  ARRatting
+//
+//  Created by Rohit Makwana on 21/04/20.
+//  Copyright © 2020 Rohit Makwana. All rights reserved.
+//
+
+import SwiftUI
+
+struct RattingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RattingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RattingView()
+    }
+}
