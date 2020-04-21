248a52dfbac6f41583965805e7aa28a9b78493a1
Rohit Makwana <>
2020-04-21 15:35:24 +0530

    first commit

---
+++ ARRatting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ARRatting
+//
+//  Created by Rohit Makwana on 21/04/20.
+//  Copyright © 2020 Rohit Makwana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
